d4182c800b28f5fa90e5222a2968590eae2230d3
Carlos Pava <cpava@condorlabs.io>
2021-05-24 22:34:33 -0500

    set image to launch screen and set colors.

--- MercadoLibre/MercadoLibre/Helpers/UIColor+Color.swift
+++ MercadoLibre/MercadoLibre/Helpers/UIColor+Color.swift
@@ -8,6 +8,6 @@
 import UIKit
 
 extension UIColor {
-    static var yellowML: UIColor = UIColor(red: 255/255, green: 240/255, blue: 89/255, alpha: 1)
-    static var adviceML: UIColor = UIColor(red: 87/255, green: 151/255, blue: 250/255, alpha: 1)
+    static var yellowML: UIColor = #colorLiteral(red: 1, green: 0.9411764706, blue: 0.3490196078, alpha: 1)
+    static var adviceML: UIColor = #colorLiteral(red: 0.3411764706, green: 0.5921568627, blue: 0.9803921569, alpha: 1)
 }
